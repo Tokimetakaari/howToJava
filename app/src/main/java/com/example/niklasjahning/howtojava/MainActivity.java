ef69d22c79ebf7ea86799f8b0c616bbe6531ef94
Tokimetakaari <tokimetakaari.zukarito@googlemail.com>
2018-08-03 15:33:52 +0200

    test 2

--- app/src/main/java/com/example/niklasjahning/howtojava/MainActivity.java
+++ app/src/main/java/com/example/niklasjahning/howtojava/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     String test = "test succesful?";
+    boolean succesful = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
