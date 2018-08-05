8302daee47950002e199dbf41326e393915e5620
Tokimetakaari <tokimetakaari.zukarito@googlemail.com>
2018-08-05 14:23:35 +0200

    Praxis aufbau

---
+++ app/src/main/java/com/example/niklasjahning/howtojava/PlayLesson.java
@@ -0,0 +1,38 @@
+package com.example.niklasjahning.howtojava;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class PlayLesson extends AppCompatActivity implements View.OnClickListener {
+
+    int layoutNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent i = getIntent();
+        Bundle data = i.getExtras();
+        layoutNumber = data.getInt("layout_num");
+        selectLayout();
+    }
+
+    private void selectLayout()
+    {
+     /*   switch (layoutNumber)
+        {
+            case 1:  setContentView();
+            break;
+            case 2: setContentView();
+            break;
+        }*/
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
--- app/src/main/java/com/example/niklasjahning/howtojava/PlayMenu.java
+++ app/src/main/java/com/example/niklasjahning/howtojava/PlayMenu.java
@@ -1,18 +1,79 @@
 package com.example.niklasjahning.howtojava;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class PlayMenu extends AppCompatActivity implements View.OnClickListener {
+
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.play_menu);}
+        setContentView(R.layout.play_menu);
+        setupButtons();
+        setupListener();
+    }
+
+    private void setupButtons()
+    {
+        b1 = findViewById(R.id.lektion_1);
+        b2 = findViewById(R.id.lektion_2);
+        b3 = findViewById(R.id.lektion_3);
+        b4 = findViewById(R.id.lektion_4);
+        b5 = findViewById(R.id.lektion_5);
+        b6 = findViewById(R.id.lektion_6);
+        b7 = findViewById(R.id.lektion_7);
+        b8 = findViewById(R.id.lektion_8);
+        b9 = findViewById(R.id.lektion_9);
+        b10 = findViewById(R.id.lektion_10);
+    }
+
+    private void setupListener()
+    {
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        b4.setOnClickListener(this);
+        b5.setOnClickListener(this);
+        b6.setOnClickListener(this);
+        b7.setOnClickListener(this);
+        b8.setOnClickListener(this);
+        b9.setOnClickListener(this);
+        b10.setOnClickListener(this);
+    }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View view)
+    {
+        Intent i = new Intent(PlayMenu.this, PlayLesson.class);
 
+        switch (view.getId())
+        {
+            case R.id.lektion_1: i.putExtra("layout_num",1);
+                break;
+            case R.id.lektion_2: i.putExtra("layout_num",2);
+                break;
+            case R.id.lektion_3: i.putExtra("layout_num",3);
+                break;
+            case R.id.lektion_4: i.putExtra("layout_num",4);
+                break;
+            case R.id.lektion_5: i.putExtra("layout_num",5);
+                break;
+            case R.id.lektion_6: i.putExtra("layout_num",6);
+                break;
+            case R.id.lektion_7: i.putExtra("layout_num",7);
+                break;
+            case R.id.lektion_8: i.putExtra("layout_num",8);
+                break;
+            case R.id.lektion_9: i.putExtra("layout_num",9);
+                break;
+            case R.id.lektion_10: i.putExtra("layout_num",10);
+                break;
+        }
+        startActivity(i);
     }
 }
