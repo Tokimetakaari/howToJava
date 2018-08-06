0b59ba9038f8db7c7140a6f5c559e1f0b1441763
Tokimetakaari <tokimetakaari.zukarito@googlemail.com>
2018-08-06 13:01:22 +0200

    Praxis aufbau

--- app/src/main/java/com/example/niklasjahning/howtojava/TheoryMenu.java
+++ app/src/main/java/com/example/niklasjahning/howtojava/TheoryMenu.java
@@ -13,7 +13,7 @@ public class TheoryMenu extends AppCompatActivity implements View.OnClickListene
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.theorie);
+        setContentView(R.layout.theory_menu);
         setupButtons();
         setupListener();
     }
