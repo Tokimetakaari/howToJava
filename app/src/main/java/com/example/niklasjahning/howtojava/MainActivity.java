e1cee0771169af369208c858bf71308072d93077
Kai Aslan <kai.aslan@web.de>
2018-09-04 17:59:15 +0200

    Anpassung

--- app/src/main/java/com/example/niklasjahning/howtojava/MainActivity.java
+++ app/src/main/java/com/example/niklasjahning/howtojava/MainActivity.java
@@ -1,9 +1,5 @@
 package com.example.niklasjahning.howtojava;
 
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.content.Intent;
 import android.support.v4.view.GestureDetectorCompat;
@@ -12,15 +8,12 @@ import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button theory, play, settings;
     private GestureDetectorCompat gestureObject;
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
-    NavigationView burger;
     private Intent i;
 
     @Override
@@ -28,30 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gestureObject = new GestureDetectorCompat(this, new LearnGesture());
-        setupDrawer();
+
         setupButtons();
         setupListener();
-    }
-
-    private void setupDrawer() {
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.burgerLayout);
-        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(mToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-
-
-
-
-        return super.onOptionsItemSelected(item);
     }
 
 
@@ -60,34 +32,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         theory = findViewById(R.id.theory_button);
         play = findViewById(R.id.play_button);
         settings = findViewById(R.id.settings_button);
-        connectBurger();
+
     }
 
-    private void connectBurger() {
-        burger = findViewById(R.id.test);
-        burger.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.play_menu:
 
-                        i = new Intent(MainActivity.this, PlayMenu.class);
-                        startActivity(i);
-
-                    case R.id.theory_menu:
-                        i = new Intent(MainActivity.this, TheoryMenu.class);
-                        startActivity(i);
-                        break;
-                    case R.id.setting_menu:
-                        i = new Intent(MainActivity.this, SettingsMenu.class);
-                        startActivity(i);
-                    case R.id.credits:
-                        Toast.makeText(getApplicationContext(),"Thanks for playing!",Toast.LENGTH_SHORT).show();
-                }
-                return true;
-            }
-        });
-    }
 
     private void setupListener()
     {
