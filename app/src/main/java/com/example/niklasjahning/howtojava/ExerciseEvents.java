91ca8efe0a2a5e804a26482b4e753f32f99c18eb
GermanAngel <j.gruenler@gmx.de>
2018-09-08 16:36:43 +0200

    Interfaces Exercise Class und Events Exercise Class

---
+++ app/src/main/java/com/example/niklasjahning/howtojava/ExerciseEvents.java
@@ -0,0 +1,245 @@
+package com.example.niklasjahning.howtojava;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ExerciseEvents extends AppCompatActivity implements View.OnClickListener {
+
+
+    MediaPlayer mySound;
+    TextView textView;
+    EditText editText1, editText2, editText3, editText4, editText5, editText6, editText7, editText8;
+    Button submit;
+    int i = 0;
+    int numOfCorrectAnswers = 0;
+
+    String solution1 = "Keyevent";
+    String alternative1_1 = "keyevent";
+    String alternative1_2 = "key event";
+    String alternative1_3 = "Key event";
+    String alternative1_4 = "Key Event";
+    String solution2 = "Observer";
+    String alternative2_1 = "observer";
+    String solution3 = "Observer";
+    String alternative3_1 = "observer";
+    String alternative3_2 = "Listener";
+    String alternative3_3 = "listener";
+    String solution4 = "Interface";
+    String alternative4_1 = "interface";
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    NavigationView burger;
+    private Intent intent;
+    //Hier die Strings für die Notification festlegen
+    String title = "Congrats";
+    String message = "Du hast Übung 1 bestanden";
+    private NotificationHelper nHelper;
+    private int questionsQ = 4;
+
+
+    boolean[] answerCorrect = new boolean[4];
+    boolean[] answered = new boolean[4];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cloze_default);
+        setupDrawer();
+        connectBurger();
+        setupItems();
+        setText();
+
+
+    }
+
+    private void setText() {
+        switch (i) {
+            case 0:
+                textView.setText("Wenn der Nutzer innerhalb des geöffneten Anwendungsfenster einen 'Key' drückt, ist dies ein ________ .");
+                editText2.setVisibility(View.GONE);
+                editText3.setVisibility(View.GONE);
+                editText4.setVisibility(View.GONE);
+                editText5.setVisibility(View.GONE);
+                editText6.setVisibility(View.GONE);
+                editText7.setVisibility(View.GONE);
+                editText8.setVisibility(View.GONE);
+                break;
+            case 1:
+                textView.setText("Die Objekte, welche auf die Events lauschen heißen Listener oder ________ .");
+                break;
+            case 2:
+                textView.setText("Das Observer-Pattern besagt, dass ein Key/ Mouse eine Nachricht an alle ________ sendet.");
+                break;
+            case 3:
+                textView.setText("Die Listenerklasse braucht passende Methoden, um die Events empfangen und darauf reagieren zu können, deshalb sollte man immer das _______ implementieren");
+                break;
+            case 4:
+                for (int j = 0; j < 4; j++)
+                {
+                    if (answerCorrect[j])
+                    {numOfCorrectAnswers ++;}
+                }
+                textView.setText("Sie haben " + numOfCorrectAnswers +" richtig beantwortet");
+                editText1.setVisibility(View.INVISIBLE);
+                editText1.setClickable(false);
+                i++;
+                break;
+
+
+        }
+        resetEdit();
+    }
+
+    private void setupItems() {
+
+        textView = findViewById(R.id.cloze_text);
+        editText1 = findViewById(R.id.cloze_answer_1);
+        submit = findViewById(R.id.cloze_submit_button);
+        submit.setOnClickListener(this);
+        editText2 = findViewById(R.id.cloze_answer_2);
+        editText3 = findViewById(R.id.cloze_answer_3);
+        editText4 = findViewById(R.id.cloze_answer_4);
+        editText5 = findViewById(R.id.cloze_answer_5);
+        editText6 = findViewById(R.id.cloze_answer_6);
+        editText7 = findViewById(R.id.cloze_answer_7);
+        editText8 = findViewById(R.id.cloze_answer_8);
+
+    }
+
+
+    private void connectBurger() {
+        burger = findViewById(R.id.test);
+        burger.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.play_menu:
+                        intent = new Intent(ExerciseEvents.this, PlayMenu.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.theory_menu:
+                        intent = new Intent(ExerciseEvents.this, TheoryMenu.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.setting_menu:
+                        intent = new Intent(ExerciseEvents.this, SettingsMenu.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.moveToTheory:
+                        finish();
+                        intent = new Intent(ExerciseEvents.this, InterfacesAndEvents.class);
+                        intent.putExtra("txt_num", 81);
+                        startActivity(intent);
+                        break;
+                    case R.id.credits:
+                        Toast.makeText(getApplicationContext(),"Thanks for playing!",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });}
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.burgerLayout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.cloze_submit_button) {
+            if ((!editText1.getText().toString().isEmpty()) && (i <= 4)) {
+                checkCorrectAnswers();
+                answered[i] = true;
+                i++;
+                setText();
+
+            } else if (i > 4) {
+                if (numOfCorrectAnswers >=  questionsQ /2) {
+                    mySound.start();
+                    sendNotification(title, message);
+                }
+                finish();
+
+            }
+        }
+    }
+
+    private void checkCorrectAnswers() {
+
+//        Falls mehrere Schreibweise zählen einfach eine weitere Bedingung mit || hinzufügen editText1.getText().toString().equals(alternative1)
+
+        switch (i) {
+            case 0:
+                if (editText1.getText().toString().equals(solution1) || (editText1.getText().toString().equals(alternative1_1)) || (editText1.getText().toString().equals(alternative1_2)) || (editText1.getText().toString().equals(alternative1_3)) || (editText1.getText().toString().equals(alternative1_4))) {
+                    answerCorrect[i] = true;
+                }
+            case 1:
+                if ((editText1.getText().toString().equals(solution2)) || (editText1.getText().toString().equals(alternative2_1)) ) {
+                    answerCorrect[i] = true;
+                }
+                break;
+            case 2:
+                if (editText1.getText().toString().equals(solution3) || (editText1.getText().toString().equals(alternative3_1)) || (editText1.getText().toString().equals(alternative3_2)) || (editText1.getText().toString().equals(alternative3_3)) ) {
+                    answerCorrect[i] = true;
+
+                }
+                break;
+            case 3:
+                if (editText1.getText().toString().equals(solution4) || (editText1.getText().toString().equals(alternative4_1)) ) {
+                    answerCorrect[i] = true;
+
+                }
+                break;
+            default:
+                break;
+
+
+
+        }
+
+    }
+
+    private void resetEdit()
+    {
+        editText1.setText("");
+    }
+
+
+    public void sendNotification(String title, String message) {
+        NotificationCompat.Builder nBuilder = nHelper.getChannelNotification(title, message);
+        nHelper.getNotificationManager().notify(1, nBuilder.build());
+    }
+
+
+}
--- app/src/main/java/com/example/niklasjahning/howtojava/ExerciseInterfaces.java
+++ app/src/main/java/com/example/niklasjahning/howtojava/ExerciseInterfaces.java
@@ -1,4 +1,215 @@
 package com.example.niklasjahning.howtojava;
 
-public class ExerciseInterfaces  {
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ExerciseInterfaces extends AppCompatActivity implements View.OnClickListener {
+
+    MediaPlayer mySound;
+    TextView textView;
+    CheckBox box1, box2, box3, box4;
+    Button submit;
+    int i = 0;
+    int numOfCorrectAnswers=0;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    NavigationView burger;
+    private Intent intent;
+    //Hier die Strings für die Notification festlegen
+    String title = "Congrats";
+    String message = "Du hast Übung 1 bestanden";
+    private NotificationHelper nHelper;
+    private int questionsQ = 2;
+
+    boolean[] answerCorrect = new boolean[2];
+    boolean[] answered = new boolean[2];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkbox_layout_default);
+        setupItems();
+        setupDrawer();
+        connectBurger();
+        mySound = MediaPlayer.create(this,R.raw.sound);
+        setText();
+    }
+
+    private void setupItems() {
+        textView = findViewById(R.id.checkbox_layout_4_question);
+        box1 = findViewById(R.id.checkbox_layout_4_checkbox1);
+        box2 = findViewById(R.id.checkbox_layout_4_checkbox2);
+        box3 = findViewById(R.id.checkbox_layout_4_checkbox3);
+        box4 = findViewById(R.id.checkbox_layout_4_checkbox4);
+        submit = findViewById(R.id.checkbox_submit_button);
+        nHelper = new NotificationHelper(this);
+        submit.setOnClickListener(this);
+    }
+
+    private void connectBurger() {
+        burger = findViewById(R.id.test);
+        burger.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.play_menu:
+                        intent = new Intent(ExerciseInterfaces.this, PlayMenu.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.theory_menu:
+                        intent = new Intent(ExerciseInterfaces.this, TheoryMenu.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.setting_menu:
+                        intent = new Intent(ExerciseInterfaces.this, SettingsMenu.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.moveToTheory:
+                        finish();
+                        intent = new Intent(ExerciseInterfaces.this, InterfacesAndEvents.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.credits:
+                        Toast.makeText(getApplicationContext(),"Thanks for playing!",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.burgerLayout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view.getId() == R.id.checkbox_submit_button)
+        {
+            if ( (box1.isChecked()|| box2.isChecked() || box3.isChecked() || box4.isChecked()) && (i <=2))
+            {
+                checkCorrectAnswers();
+                answered[i] = true;
+                i++;
+                setText();
+            }
+            else if (i>2)
+            {
+                if (numOfCorrectAnswers >=  questionsQ /2) {
+                    mySound.start();
+                    sendNotification(title, message);
+                }
+                finish();
+            }
+        }
+    }
+
+    private void setText() {
+        switch (i) {
+            case 0:
+                textView.setText("Welche der folgenden Punkte treffen auf Interfaces zu?");
+                box1.setText("Ein Interfaces ist, ähnlich wie eine Klasse, ein Referenztyp.");
+                box2.setText("Interfaces werden durch das Schlüsselwort 'extends' implementiert.");
+                box3.setText("Man kann immer nur ein Interface implementieren.");
+                box4.setText("Wenn eine Klasse ein Interface implementiert hat, muss diese alle Methodensignaturen übernehmen/ überschreiben.");
+                break;
+            case 1:
+                textView.setText("Implementiert eine Klasse ein Interface schreibt man: public class MyClass implements MyInterface {}");
+                box1.setText("wahr");
+                box2.setText("falsch");
+                box3.setVisibility(View.GONE);
+                box4.setVisibility(View.GONE);
+                break;
+            case 2:
+                countCorrectAnswers();
+                textView.setText("Sie haben " + numOfCorrectAnswers + " Fragen von " + questionsQ + "richtig beantwortet.");
+                box1.setClickable(false);
+                box1.setVisibility(View.INVISIBLE);
+                box2.setClickable(false);
+                box2.setVisibility(View.INVISIBLE);
+                box3.setClickable(false);
+                box3.setVisibility(View.INVISIBLE);
+                box4.setClickable(false);
+                box4.setVisibility(View.INVISIBLE);
+                submit.setText("Zurück zum Hauptmenü");
+                i++;
+
+                break;
+            default:
+                break;
+        }
+        resetCheckbox();
+    }
+
+
+    private void resetCheckbox ()
+    {
+        box1.setChecked(false);
+        box2.setChecked(false);
+        box3.setChecked(false);
+        box4.setChecked(false);
+    }
+
+    private int countCorrectAnswers() {
+        for (int j = 0; j < 2; j++)
+        {
+            if (answerCorrect[j])
+            {numOfCorrectAnswers ++;}
+        }
+        return numOfCorrectAnswers;
+    }
+
+    private void checkCorrectAnswers() {
+        switch (i)
+        {
+            case 0: if (box1.isChecked() && !box2.isChecked() && !box3.isChecked() && box4.isChecked())
+            {
+                answerCorrect[i] = true;
+            }
+                break;
+            case 1: if (box1.isChecked() && !box2.isChecked())
+            {
+                answerCorrect[i] = true;
+            }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void sendNotification(String title, String message) {
+        NotificationCompat.Builder nBuilder = nHelper.getChannelNotification(title, message);
+        nHelper.getNotificationManager().notify(1, nBuilder.build());
+    }
+
 }
+
